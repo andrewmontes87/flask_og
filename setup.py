1872962b6e1f47a9c70cc0ac3abb72b745831a06
andrewmontes87 <andrewmontes87@gmail.com>
2016-09-06 19:30:17 -0700

    1.0.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='flask_og',
-    version='1.0.1',
+    version='1.0.2',
     description='Flask server and d3 app for oil gas visualizer',
     author='Andrew Montes',
     author_email='andrewmontes87@gmail.com',    
